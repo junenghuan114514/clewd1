c2fa2415b26b26ca8839e17ed58dcd27275de30e
daociyiyou <151892595+junenghuan114514@users.noreply.github.com>
2024-02-04 13:20:46 +0000

    1

---
+++ config.js
@@ -0,0 +1,52 @@
+/*
+* https://gitgud.io/ahsk/clewd
+* https://github.com/h-a-s-k/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "SET YOUR COOKIE HERE",
+    "Ip": "127.0.0.1",
+    "Port": 8444,
+    "BufferSize": 8,
+    "SystemInterval": 3,
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": false,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": false,
+        "PreserveChats": false,
+        "LogMessages": false,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
