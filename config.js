484b4f291443102215916b9f1fb1254f03025994
daociyiyou <151892595+junenghuan114514@users.noreply.github.com>
2024-02-04 13:21:50 +0000

    1

--- config.js
+++ config.js
@@ -1,37 +1,38 @@
 /*
-* https://gitgud.io/ahsk/clewd
-* https://github.com/h-a-s-k/clewd
-*/
+ * https://gitgud.io/ahsk/clewd
+ * https://github.com/h-a-s-k/clewd
+ */
 
 // SET YOUR COOKIE BELOW
 
 module.exports = {
-    "Cookie": "SET YOUR COOKIE HERE",
-    "Ip": "127.0.0.1",
-    "Port": 8444,
-    "BufferSize": 8,
-    "SystemInterval": 3,
-    "PromptExperimentFirst": "",
-    "PromptExperimentNext": "",
-    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
-    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
-    "Settings": {
-        "RenewAlways": true,
-        "RetryRegenerate": false,
-        "PromptExperiments": true,
-        "SystemExperiments": true,
-        "PreventImperson": false,
-        "AllSamples": false,
-        "NoSamples": false,
-        "StripAssistant": false,
-        "StripHuman": false,
-        "PassParams": false,
-        "ClearFlags": false,
-        "PreserveChats": false,
-        "LogMessages": false,
-        "Superfetch": true
-    }
-}
+  Cookie:
+    "sessionKey=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,sessionKey=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",
+  Ip: "127.0.0.1",
+  Port: 8444,
+  BufferSize: 8,
+  SystemInterval: 3,
+  PromptExperimentFirst: "",
+  PromptExperimentNext: "",
+  PersonalityFormat: "{{char}}'s personality: {{personality}}",
+  ScenarioFormat: "Dialogue scenario: {{scenario}}",
+  Settings: {
+    RenewAlways: true,
+    RetryRegenerate: false,
+    PromptExperiments: true,
+    SystemExperiments: true,
+    PreventImperson: false,
+    AllSamples: false,
+    NoSamples: false,
+    StripAssistant: false,
+    StripHuman: false,
+    PassParams: false,
+    ClearFlags: false,
+    PreserveChats: false,
+    LogMessages: false,
+    Superfetch: true,
+  },
+};
 
 /*
  BufferSize
